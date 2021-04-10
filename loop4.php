43665dda21d6f88372f76bffb1f9df9b8db5b262
dishasharma2801 <60421033+dishasharma2801@users.noreply.github.com>
2021-04-10 16:17:21 +0530

    Add files via upload

---
+++ loop4.php
@@ -0,0 +1,27 @@
+     <html> 
+     <head> 
+  <title></title>
+  </head>
+  <body> 
+ 
+   <table width="200px" cellspacing="0px" cellpadding="0px" border="1px">
+      <?php
+      for($row=1;$row<=6;$row++)
+	  {
+          echo "<tr>";
+          for($col=1;$col<=6;$col++)
+		  {
+          $total=$row+$col;
+          if($total%2==0)
+		  {
+          echo "<td height=30px width=30px bgcolor=#FFFFFF></td>";
+          }
+		  else
+		  {
+          echo "<td height=30px width=30px bgcolor=#000000></td>";
+          }
+          }
+          echo "</tr>";
+    }
+          ?>
+  
